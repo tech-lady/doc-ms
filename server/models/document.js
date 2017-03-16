79894f5fb983337b0a724bba7ca6461b9f0a9b41
I-Diva <olayemifolakemi@yahoo.com>
2017-03-16 20:31:05 +0100

    database(models): generate model for document

--- server/models/document.js
+++ server/models/document.js
@@ -1,13 +1,20 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var document = sequelize.define('document', {
-    title: DataTypes.STRING
+  var Document = sequelize.define('Document', {
+    title: DataTypes.STRING,
+    access: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    ownerId: DataTypes.INTEGER,
+    ownerRoleId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        Document.belongsTo(models.User, {
+          foreignKey: 'ownerId'
+        });
       }
     }
   });
-  return document;
+  return Document;
 };
