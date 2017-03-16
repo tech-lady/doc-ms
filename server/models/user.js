cdf5dd8f257cd93657b92475d2a85dd1e74e8e46
I-Diva <olayemifolakemi@yahoo.com>
2017-03-16 20:32:26 +0100

    database(models): generate model for user

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,23 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var user = sequelize.define('user', {
-    title: DataTypes.STRING
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    name: DataTypes.JSON,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    roleId: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        User.hasMany(models.Document, {
+            foreignKey: 'ownerId'
+          }),
+          User.belongsTo(models.Role, {
+            foreignKey: 'roleId'
+          })
       }
     }
   });
-  return user;
+  return User;
 };
