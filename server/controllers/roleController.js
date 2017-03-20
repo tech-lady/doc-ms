de89de459cd0db069844607967b0ef017b526a36
I-Diva <olayemifolakemi@yahoo.com>
2017-03-20 15:22:34 +0100

    refactor(crud): update the methods on role controller

--- server/controllers/roleController.js
+++ server/controllers/roleController.js
@@ -1,63 +1,48 @@
-const role = require('../models').role;
+const db = require('../models');
 
-module.exports = {
-  create(req, res) {
-    return role
-      .create({
-        title: req.body.title,
-        roleId: req.params.todoId,
-      })
-      .then(role => res.status(201).send(role))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.createRole = (req, res) => {
+  const newRole = {
+    title: req.body.title
+  };
+  db.Role.create(newRole)
+    .then(role => res.status(201).send(role))
+    .catch(error => res.status(400).send(error));
+}
 
-module.exports = {
-  find(req, res) {
-    return role
-      .find({
-        title: req.body.title,
-        roleId: req.params.todoId,
-      })
-      .then(role => res.status(201).send(role))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.getRole = (req, res) => {
+  db.Role.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((role, err) => {
+      res.status(200).send(role)
+    })
+}
 
+module.exports.getRoles = (req, res) => {
+  db.Role.findAll()
+    .then((roles, err) => {
+      res.status(200).send(roles)
+    })
+}
 
-module.exports = {
-  edit(req, res) {
-    return role
-      .edit({
-        title: req.body.title,
-        roleId: req.params.todoId,
-      })
-      .then(role => res.status(201).send(role))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.updateRole = (req, res) => {
+  db.Role.findById({
+      where: {
+        title: req.body.title
+      }
+    })
+    .then(role => res.status(201).send(role))
+    .catch(error => res.status(400).send(error));
+}
 
-
-module.exports = {
-  update(req, res) {
-    return role
-      .update({
-        title: req.body.title,
-        roleId: req.params.todoId,
-      })
-      .then(role => res.status(201).send(role))
-      .catch(error => res.status(400).send(error));
-  },
-};
-
-module.exports = {
-  delete(req, res) {
-    return role
-      .delete({
-        title: req.body.title,
-        roleId: req.params.todoId,
-      })
-      .then(role => res.status(201).send(role))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.deleteRole = (req, res) => {
+  db.Role.findById({
+      where: {
+        title: req.body.title
+      }
+    })
+    .then(role => res.status(201).send(role))
+    .catch(error => res.status(400).send(error));
+}
