bb7c9cbf9b0ec892b6dc9fff3ba191b262379d8d
I-Diva <olayemifolakemi@yahoo.com>
2017-03-20 15:25:24 +0100

    refactor(schema): update the tables for role feature

--- server/models/role.js
+++ server/models/role.js
@@ -1,10 +1,7 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Role = sequelize.define('Role', {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
-      primaryKey: true,
     }
   }, {
     classMethods: {
