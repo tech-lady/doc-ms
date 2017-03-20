c2f298931b41b707c18a8780a9ba77af96ec9e48
I-Diva <olayemifolakemi@yahoo.com>
2017-03-20 15:23:04 +0100

    refactor(crud): update the methods on user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,76 +1,113 @@
-const user = require('../models').user;
+const user = require('../models');
 
-module.exports = {
-  create(req, res) {
-    return user
-      .create({
-        username: newUser.username,
-        name: newUser.name,
-        email: newUser.email,
-        password,
-        roleId: newUser.roleId
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-};
 
-module.exports = {
-  edit(req, res) {
-    return user
-      .create({
-        username: newUser.username,
-        name: newUser.name,
-        email: newUser.email,
-        password,
-        roleId: newUser.roleId
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.createUser = (req, res) => {
+  const newUser = {
+    UserId: user.id,
+    Email: user.email,
+    RoleId: user.RoleId
+  }
+  db.User.create(newUser)
+    .then(user => res.status(201).send(user))
+    .catch(error => res.status(400).send(error));
+}
 
-module.exports = {
-  get(req, res) {
-    return user
-      .get({
-        username: newUser.username,
-        name: newUser.name,
-        email: newUser.email,
-        password,
-        roleId: newUser.roleId
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-};
+module.exports.getUser =
 
-module.exports = {
-  update(req, res) {
-    return user
-      .update({
-        username: newUser.username,
-        name: newUser.name,
-        email: newUser.email,
-        password,
-        roleId: newUser.roleId
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-};
 
-module.exports = {
-  delete(req, res) {
-    return user
-      .delete({
-        username: newUser.username,
-        name: newUser.name,
-        email: newUser.email,
-        password,
-        roleId: newUser.roleId
-      })
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
-};
+
+
+
+
+
+  // /get: function(req, res) {
+  // //     res.send('User route');
+  // //     User.findById(req.params.id, function(err, user) {
+  // //       if (err) {
+  // //         res.status(400).json({ 'message': 'Something wrong'});
+  // //       } else {
+  // //         res.json(user);
+  // //       }
+  // //     });
+  //  },
+
+
+  //   store: function(req, res) {
+  //     User.create()
+  //   }
+  // }*/
+
+
+  // module.exports = {
+  //   create: (req, res) => {
+  //     res.send('User route');
+  //     .create({
+  //         username: newUser.username,
+  //         name: newUser.name,
+  //         email: newUser.email,
+  //         password,
+  //         roleId: newUser.roleId
+  //       })
+  //       .then(user => res.status(201).send(user))
+  //       .catch(error => res.status(400).send(error));
+  //   },
+
+
+
+  //   edit: (req, res) => {
+  //     return user
+  //       .create({
+  //         username: newUser.username,
+  //         name: newUser.name,
+  //         email: newUser.email,
+  //         password,
+  //         roleId: newUser.roleId
+  //       })
+  //       .then(user => res.status(201).send(user))
+  //       .catch(error => res.status(400).send(error));
+  //   },
+
+
+
+  //   get: (req, res) => {
+  //     return user
+  //       .get({
+  //         username: newUser.username,
+  //         name: newUser.name,
+  //         email: newUser.email,
+  //         password,
+  //         roleId: newUser.roleId
+  //       })
+  //       .then(user => res.status(201).send(user))
+  //       .catch(error => res.status(400).send(error));
+  //   },
+
+
+
+  //   update: (req, res) => {
+  //     return user
+  //       .update({
+  //         username: newUser.username,
+  //         name: newUser.name,
+  //         email: newUser.email,
+  //         password,
+  //         roleId: newUser.roleId
+  //       })
+  //       .then(user => res.status(201).send(user))
+  //       .catch(error => res.status(400).send(error));
+  //   },
+
+
+  //   delete: (req, res) => {
+  //     return user
+  //       .delete({
+  //         username: newUser.username,
+  //         name: newUser.name,
+  //         email: newUser.email,
+  //         password,
+  //         roleId: newUser.roleId
+  //       })
+  //       .then(user => res.status(201).send(user))
+  //       .catch(error => res.status(400).send(error));
+  //   },
+  // };
