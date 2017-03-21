6d8387504cb97111b8f930a352a81ed235fcdc1c
I-Diva <olayemifolakemi@yahoo.com>
2017-03-21 01:57:58 +0100

    update(express): fix controller issues

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,23 @@
-import user from '../models';
-
+import db from '../models';
 
 export const createUser = (req, res) => {
-  const newUser = {
-    UserId: user.id,
-    Email: user.email,
-    RoleId: user.RoleId
-  }
-  db.User.create(newUser)
+  db.User
+    .create(req.body)
     .then(user => res.status(201).send(user))
     .catch(error => res.status(400).send(error));
 }
 
 export const getUser = (req, res) => {
   db.User.findById(req.params.id)
-    .then((User, err) => {
-      res.status(200).send(User);
-    })
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(400).json(error));
 }
 
+export const getUsers = (req, res) => {
+  db.user.findAll({})
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(400).json(error));
+}
 
 export const updateUser = (req, res) => {
   db.User.findById(req.params.id)
@@ -29,15 +28,13 @@ export const updateUser = (req, res) => {
       password,
       roleId: User.roleId
     })
-    .then((User, err) => {
-      res.status(200).send(User);
-    })
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(400).json(error));
 }
 
 
 export const deleteUser = (req, res) => {
   db.User.findById(req.params.id)
-    .then((User, err) => {
-      res.status(200).send(User);
-    })
+    .then(user => res.status(200).json(user))
+    .catch(error => res.status(400).json(error));
 }
