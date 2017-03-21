16f020bf574b4e8c7f06d013b9ac3b11a0f4d30d
I-Diva <olayemifolakemi@yahoo.com>
2017-03-21 01:59:25 +0100

    update(rotes): update feature routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@
 // require('./user.js')(app);
 import roleRouter from './role.js';
 import docRouter from './document.js';
+import userRouter from './user.js';
 // })();
 export default {
   roleRouter,
-  docRouter
+  docRouter,
+  userRouter
 }
