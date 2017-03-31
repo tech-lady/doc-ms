4613d8b6be9c3b2fa749d967bb0727a9f070846f
I-Diva <olayemifolakemi@yahoo.com>
2017-03-31 04:28:36 +0100

    tests(auth): generate tests for authentication

--- server/tests/middleware/auth.spec.js
+++ server/tests/middleware/auth.spec.js
@@ -1,17 +1,17 @@
-import supertest from 'supertest';
-import should from 'should';
-import chai from 'chai';
-import { userDetail, documentDetail } from '../testFile.js';
-// import app from '../../server';
-// import db from '../../models';
+// import supertest from 'supertest';
+// import should from 'should';
+// import chai from 'chai';
+// import { userDetail, documentDetail } from '../testFile.js';
+// // import app from '../../server';
+// // import db from '../../models';
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
 
-describe('Document', () => {
-  it('should run test', () => {
-    expect
-      (true).to.be.true;
-  })
+// describe('Document', () => {
+//   it('should run test', () => {
+//     expect
+//       (true).to.be.true;
+//   })
 
-});
+// });
