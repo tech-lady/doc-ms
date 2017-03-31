944588a8220a52a137d86ee86cfa8559b76f76a9
I-Diva <olayemifolakemi@yahoo.com>
2017-03-31 04:27:33 +0100

    tests(models): generate tests for models

--- server/tests/models/roles.spec.js
+++ server/tests/models/roles.spec.js
@@ -1,18 +1,18 @@
-import supertest from 'supertest';
-import should from 'should';
-import chai from 'chai';
-import { userDetail, roleDetail } from '../testFile.js';
-// import app from '../../server';
-// import db from '../../models';
+// import supertest from 'supertest';
+// import should from 'should';
+// import chai from 'chai';
+// import { userDetail, roleDetail } from '../testFile.js';
+// // import app from '../../server';
+// // import db from '../../models';
 
-const expect = chai.expect;
+// const expect = chai.expect;
 
 
-describe('Role', () => {
-  it('should run test', () => {
-    expect
-      (true).to.be.true;
-  })
+// describe('Role', () => {
+//   it('should run test', () => {
+//     expect
+//       (true).to.be.true;
+//   })
 
-});
-// const server = supertest.agent(app);
+// });
+// // const server = supertest.agent(app);
--- server/tests/models/users.spec.js
+++ server/tests/models/users.spec.js
@@ -1,17 +1,72 @@
-import supertest from 'supertest';
-import should from 'should';
-import chai from 'chai';
-import { userDetail } from '../testFile.js';
-// import app from '../../server';
+// /*eslint no-unused-expressions: "off"*/
+// import supertest from 'supertest';
+// import should from 'should';
+// import chai from 'chai';
+// import { userDetail } from '../testFile';
+// import app from '../../../server';
 // import db from '../../models';
 
-const expect = chai.expect;
 
+// const expect = chai.expect;
+// const request = supertest(app);
 
-describe('User', () => {
-  it('should run test', () => {
-    expect
-      (true).to.be.true;
-  })
-
-});
+// describe('User Model', () => {
+//   let user;
+//   describe('User validation', () => {
+//     it('should create new user', (done) => {
+//       db.User.create(user)
+//         .then((newUser) => {
+//           user = newUser;
+//           done();
+//         });
+//     });
+//     it('should be able to create a user', () => {
+//       expect(user).to.exist;
+//       expect(typeof user).to.equal('object');
+//     });
+//     it('user should have username', () => {
+//       expect(user).to.exist;
+//       expect(user).to.have.deep.property('username');
+//     });
+//     it('should create a user with first name & last name', () => {
+//       expect(user.username).to.equal(helper.username);
+//       expect(user.name).to.equal(helper.name);
+//     });
+//     it('should create a user with a valid email', () => {
+//       expect(user.email).to.equal(helper.email);
+//     });
+//     it('should ensure that username is not null', () => {
+//       db.User.create(helper.noUsername)
+//         .catch((error) => {
+//           expect(/notNull Violation: username cannot be null/
+//             .test(error.message)).to.be.true;
+//         });
+//     });
+//     it('should create a user with a defined role', (done) => {
+//       db.User.findById(user.id, {
+//           include: [db.Role]
+//         })
+//         .then((foundUser) => {
+//           expect(foundUser.Role.title).to.equal('User');
+//           done();
+//         });
+//     });
+//   });
+//   describe('Email validation', () => {
+//     it('should ensure that email is authenthic', () => {
+//       db.User.create(helper.invalidEmail)
+//         .catch((error) => {
+//           expect(/Validation error: Validation isEmail failed/
+//             .test(error.message)).to.be.true;
+//         });
+//     });
+//   });
+//   describe('Password Validation', () => {
+//     it('should be valid if compared', () => {
+//       db.User.create(helper.newUser)
+//         .then((createdUser) => {
+//           expect(createdUser.validPassword(helper.newUser.password)).to.be.true;
+//         });
+//     });
+//   });
+// });
