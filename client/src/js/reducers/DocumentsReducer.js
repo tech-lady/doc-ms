6540a301aba2c099fa2b1477b9065698c2b1391f
I-Diva <olayemifolakemi@yahoo.com>
2017-03-31 07:25:36 +0100

    feature: add method that gets documents

--- client/src/js/reducers/DocumentsReducer.js
+++ client/src/js/reducers/DocumentsReducer.js
@@ -2,12 +2,12 @@ import * as types from '../actions/Types';
 
 export default (state = [], action) => {
   switch (action.type) {
-  case types.GET_ALL_DOCUMENTS:
-    return action.documents;
-  case types.GET_DOCUMENT:
-    console.log(action.doc);
-    return [action.doc];
-  default:
-    return state;
+    case types.GET_ALL_DOCUMENTS:
+      return action.documents;
+    case types.GET_DOCUMENT:
+      console.log(action.doc);
+      return [action.doc];
+    default:
+      return state;
   }
 };
