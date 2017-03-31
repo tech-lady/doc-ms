b08bd5556bc7d73badd4f4725f6eccc6fb30883c
I-Diva <olayemifolakemi@yahoo.com>
2017-03-31 04:19:34 +0100

    chore(auth): update user authentication details

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -1,54 +1,52 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import db from '../models';
+
+dotenv.config({ silence: true });
 
 /**
  * Secret token for jsonwebtoken
  */
 const secret = process.env.SECRET;
 
-const Authentication = {
-
-  /**
-   * verifyToken - Verifies if a token supplied or not is valid
-   *
-   * @param  {Object} req  Request Object
-   * @param  {Object} res  Response Object
-   * @param  {Object} next
-   * @returns {Object} Response status
-   */
-  verifyToken: (req, res, next) => {
-    const token = req.headers.authorization || req.headers['x-access-token'];
-    if (!token) {
-      return res.status(401).send({ message: 'Authentication required to access this route!' });
-    }
-
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        return res.status(401).send({ message: 'Authentication failed due to invalid token!' });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  },
-
-  /**
-   * verifyAdmin - Verifies that the user role is supplied is an admin
-   *
-   * @param  {Object} req  Request Object
-   * @param  {Object} res  Response Object
-   * @param  {Object} next
-   * @returns {Object} Response Object
-   */
-  verifyAdmin: (req, res, next) => {
-    db.Role.findById(req.decoded.RoleId)
-      .then((role) => {
-        if (role.title === 'Admin') {
-          next();
-        } else {
-          return res.status(403).send({ message: 'Access forbidden, you are not an admin!' });
-        }
-      });
+/**
+ * verifyToken - Verifies if a token supplied or not is valid
+ *
+ * @param  {Object} req  Request Object
+ * @param  {Object} res  Response Object
+ * @param  {Object} next
+ * @returns {Object} Response status
+ */
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization || req.headers['x-access-token'];
+  if (!token) {
+    return res.status(401).json({ message: 'Authentication required to access this route!' });
   }
+
+  jwt.verify(token, secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Authentication failed due to invalid token!' });
+    }
+    req.decoded = decoded;
+    next();
+  });
 };
 
-export default Authentication;
+/**
+ * verifyAdmin - Verifies that the user role is supplied is an admin
+ *
+ * @param  {Object} req  Request Object
+ * @param  {Object} res  Response Object
+ * @param  {Object} next
+ * @returns {Object} Response Object
+ */
+export const verifyAdmin = (req, res, next) => {
+  db.Role.findById(req.decoded.roleId)
+    .then((role) => {
+      if (role.title === 'admin') {
+        next();
+      } else {
+        return res.status(403).send({ message: 'Access forbidden, you are not an admin!' });
+      }
+    });
+};
