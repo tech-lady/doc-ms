4e9c040dacd549a81ee4a503c20497219473d494
I-Diva <olayemifolakemi@yahoo.com>
2017-04-02 06:05:59 +0100

    tests(models): generate tests for models

--- server/tests/models/documents.spec.js
+++ server/tests/models/documents.spec.js
@@ -1,44 +1,45 @@
 // /* eslint no-unused-expressions: "off"*/
 // import chai from 'chai';
 // import chaiHttp from 'chai-http';
-// import newData from '../testFile';
+// import { userDetail, documentDetail } from '../testFile';
 // import app from '../../../server';
 // import db from '../../models';
 
 // const expect = chai.expect;
 // const should = chai.should();
 // chai.use(chaiHttp);
-// // const request = supertest(app);
+// //const request = supertest(app);
 // const request = chai.request(app);
 
 
 // process.env.NODE_ENV = 'test';
 
 // const User = db.Users;
-// const Document = db.Documents;
-// // const adminUser = newData.adminUser;
-// const regUser = newData.newUser;
-// const publicDocument = newData.publicDoc;
+// const Document = db.Document;
+// // const adminUser = userDetail[1];
+// // const regUser = userDetail[2];
+// const publicDocument = documentDetail[3];
 
 // describe('Document Model', () => {
 //   let docData;
 //   let userdata;
 
-//   before((done) => {
-//     User.create(regUser)
+//     Document.create(documentDetail[3])
 //       .then((newUser) => {
 //         userdata = newUser;
 //         publicDocument.userId = userdata.id;
 //         publicDocument.role = String(userdata.roleId);
 //         done();
 //       });
-//   });
+
+// after(() => Document.destroy({ where: {} }));
 
 //   describe('Create Document', () => {
 //     it('should create new document', (done) => {
 //       Document.create(publicDocument)
 //         .then((newDocument) => {
 //           docData = newDocument;
+//           document.should.have.property('ownerRoleId');
 //           done();
 //         });
 //     });
