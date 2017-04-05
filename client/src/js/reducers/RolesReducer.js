4068c27a0f55b8b1188442d21017f0706c5b2af4
I-Diva <olayemifolakemi@yahoo.com>
2017-04-05 05:45:01 +0100

    create a role state and import all states

--- client/src/js/reducers/RolesReducer.js
+++ client/src/js/reducers/RolesReducer.js
@@ -2,10 +2,11 @@ import * as types from '../actions/Types';
 
 export default (state = [], action) => {
   switch (action.type) {
-    case types.GET_ALL_USERS:
-      return action.users;
-    case types.GET_USER:
-      return [action.users];
+    case types.GET_ALL_ROLES:
+      return action.roles;
+    case types.GET_ROLES:
+      console.log(action.role);
+      return [action.role];
     default:
       return state;
   }
--- client/src/js/reducers/index.js
+++ client/src/js/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import documents from './DocumentsReducer';
 import authenticated from './AuthReducer';
 import users from './UsersReducers';
+import roles from './RolesReducer';
 
 export default combineReducers({
   documents,
   users,
+  roles,
   authenticated
 });
